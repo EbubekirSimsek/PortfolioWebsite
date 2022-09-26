92639bb6186232caeeb798bfde3dedab73e29132
Ebubekir Simsek <ebubekrisimsek01@hotmail.com>
2022-09-26 22:16:46 +0200

    Done welcome page

--- portfoliowebsite/src/App.js
+++ portfoliowebsite/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
-import Intro from "./components/intro/Intro";
+import Intro from "./components/intro/Intro.jsx";
+import About from "./components/about/About.jsx";
 
 function App() {
   return (
     <div>
       <Intro />
+      <About />
     </div>
   );
 }
---
+++ portfoliowebsite/src/components/about/About.jsx
@@ -0,0 +1,11 @@
+import "./about.css";
+
+const About = () => {
+    return (
+        <div className="about" >
+        About page
+        </div>
+    );
+    }
+
+export default About;
